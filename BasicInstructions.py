2af08bf1b504a88d93d832d33c237e9771fd3e27
KimJisooTurtleRabbitKim <90258690+KimJisooTurtleRabbitKim@users.noreply.github.com>
2021-09-07 17:56:36 +0530

    Add files via upload

---
+++ BasicInstructions.py
@@ -0,0 +1,13 @@
+name="pythonlang"
+marks=20
+print("Welcome to the world of python")
+print(name)
+print(marks)
+pocket_money=int(input("Enter the number guess it between (1-9): "))
+print(pocket_money)
+if (pocket_money < 3):
+    print("Too small think a bigger number maybe?")
+elif(pocket_money == 8):
+    print("Oh so close think a little more bigger number!")
+else :
+    print("Yaaas you won Great Job ")
